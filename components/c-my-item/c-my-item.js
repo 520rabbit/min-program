fff5c21fc19f4ff1ea1bf551449bf520dac21611
520rabbit <946485828@qq.com>
2020-04-07 21:29:31 +0800

    first commit

---
+++ components/c-my-item/c-my-item.js
@@ -0,0 +1,23 @@
+// components/c-my-item/c-my-item.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
